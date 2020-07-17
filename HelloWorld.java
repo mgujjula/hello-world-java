d906ccad8f8d827f2683234ff643b3fd991afc53
Madhav <gujjula.madhava@gmail.com>
2020-07-17 04:32:04 +0000

    githook Automation

--- HelloWorld.java
+++ HelloWorld.java
@@ -40,8 +40,6 @@ public class HelloWorld {
     System.out.println("Hello, Testing git intergration Madhava");
     System.out.println("Hello, Devops World");
     System.out.println("Hello, Testing Poll SCM ");
-    System.out.println("Hello, Testing Poll SCM ");
-    System.out.println("Hello, Testing Poll SCM ");
-    System.out.println("Hello,Testing poll scm automation ");
+    System.out.println("Hello,Testing  for githook AutomAtion ");
   }
 }
